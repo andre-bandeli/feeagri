320adf2c17ff6f0191129f898fbfa44052247617
andre-bandeli <andre.bandeli@gmail.com>
2025-01-01 21:27:01 -0300

    estrutura basica criada

--- feeagri/src/componentes/Header.jsx
+++ feeagri/src/componentes/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Navbar from './Navbar';  // Importando o componente Navbar
+import Navbar from './Navbar'; 
 
 
 export default function Header() {
   return (
     <div>
         <header className="header">
-      <div className="logo">
+        {/* <div className="logo">
         <img src="/logo.png" alt="Logo" className="logo-img" />
-      </div>
-      <Navbar />  {/* Inserindo o Navbar */}
+      </div> */}
+      <Navbar /> 
     </header>
     </div>
 
---
+++ feeagri/src/componentes/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar'; 
+
+
+export default function Hero() {
+  return (
+    <div>
+        <div className="Hero">
+            
+        </div>
+    </div>
+
+  );
+}
